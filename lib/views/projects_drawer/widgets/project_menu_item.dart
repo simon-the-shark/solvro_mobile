67731423b00d98d60a5ebc90fa6fbadda2481702
Szymon Kowaliński <szymon@kowalinski.dev>
2023-11-04 20:32:30 +0100

    fixed changing current project callback

--- lib/views/projects_drawer/widgets/project_menu_item.dart
+++ lib/views/projects_drawer/widgets/project_menu_item.dart
@@ -13,34 +13,33 @@ class ProjectMenuItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-      child: GestureDetector(
-        onTap: onTap,
-        child: FilterChip(
-          shape: const RoundedRectangleBorder(
-            side: BorderSide.none,
-            borderRadius: BorderRadius.all(
-              Radius.circular(12),
-            ),
+      child: FilterChip(
+        shape: const RoundedRectangleBorder(
+          side: BorderSide.none,
+          borderRadius: BorderRadius.all(
+            Radius.circular(12),
           ),
-          avatar: Icon(
-            Icons.folder,
-            color: Theme.of(context).colorScheme.inverseSurface,
-          ),
-          label: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(5),
-                child: Text(
-                  project.name,
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.inverseSurface,
-                  ),
+        ),
+        avatar: Icon(
+          Icons.folder,
+          color: Theme.of(context).colorScheme.inverseSurface,
+        ),
+        label: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: Text(
+                project.name,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.inverseSurface,
                 ),
               ),
-            ],
-          ),
-          onSelected: (bool value) {},
+            ),
+          ],
         ),
+        onSelected: (bool value) {
+          onTap();
+        },
       ),
     );
   }
