f0c3f298a98f5af8c22183c7dbe2db60fb06aa06
Szymon Kowaliński <szymon@kowalinski.dev>
2023-11-04 21:22:23 +0100

    refresh button

--- lib/views/tasks_related_views/tasks_table_view/widgets/app_bar_actions.dart
+++ lib/views/tasks_related_views/tasks_table_view/widgets/app_bar_actions.dart
@@ -1,6 +1,9 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+
+import '../../../../repositories/tasks_repository.dart';
 
 class AppBarActions extends DelegatingList<Widget> {
   AppBarActions(BuildContext context)
@@ -15,13 +18,24 @@ class AppBarActions extends DelegatingList<Widget> {
                 color: Colors.white,
               ),
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.refresh,
-                color: Colors.white,
-              ),
-            ),
+            const RefreshIcon()
           ],
         );
+}
+
+class RefreshIcon extends ConsumerWidget {
+  const RefreshIcon({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return IconButton(
+      onPressed: () {
+        ref.invalidate(tasksRepositoryProvider);
+      },
+      icon: const Icon(
+        Icons.refresh,
+        color: Colors.white,
+      ),
+    );
+  }
 }
