bd0e3ae3f4a8dd51bee27f9f3727cbbd2f6ec254
Szymon Kowaliński <szymon@kowalinski.dev>
2023-11-04 20:27:15 +0100

    fixed scrolling in drawer

--- lib/views/projects_drawer/projects_drawer.dart
+++ lib/views/projects_drawer/projects_drawer.dart
@@ -16,81 +16,88 @@ class ProjectsDrawer extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final projects = ref.watch(projectsServiceProvider).value;
-    return Drawer(
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+    Widget projectsColumn = SingleChildScrollView(
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: const [0.2, 1.0],
-                colors: [
-                  Theme.of(context).colorScheme.secondary,
-                  Theme.of(context).colorScheme.inverseSurface,
-                ],
-              ),
-            ),
-            child: SafeArea(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const SizedBox(height: 30),
-                    SizedBox(
-                      height: 80,
-                      child: Image.asset(
-                        "assets/logo_solvro.png",
-                      ),
-                    ),
-                    const SizedBox(height: 12),
-                    NameHeader(
-                      color: Theme.of(context).colorScheme.onSecondary,
-                    ),
-                    const SizedBox(height: 20),
-                    const NameListTile(),
-                    const EmailListTile(),
-                    const ProfessionListTile(),
-                    const SizedBox(height: 5),
-                    const LogoutButton(),
-                    const SizedBox(height: 25),
-                  ]),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 10),
-            child: Text(
-              "Twoje projekty: ",
-              style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  ...projects?.map(
-                        (e) => ProjectMenuItem(
-                          project: e,
-                          onTap: () {
-                            ref
-                                .read(currentProjectSubServiceProvider.notifier)
-                                .setCurrentProject(e);
-                            Scaffold.of(context).closeDrawer();
-                          },
-                        ),
-                      ) ??
-                      const [],
-                  const SizedBox(height: 10),
-                  const NewProjectButton(),
-                  const SizedBox(height: 20),
-                ],
-              ),
-            ),
-          ),
+          ...projects?.map(
+                (e) => ProjectMenuItem(
+                  project: e,
+                  onTap: () {
+                    ref
+                        .read(currentProjectSubServiceProvider.notifier)
+                        .setCurrentProject(e);
+                    Scaffold.of(context).closeDrawer();
+                  },
+                ),
+              ) ??
+              const [],
+          const SizedBox(height: 10),
+          const NewProjectButton(),
+          const SizedBox(height: 20),
         ],
       ),
     );
+    if (!isLandscape) {
+      projectsColumn = Expanded(child: projectsColumn);
+    }
+    final drawer = Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: const [0.2, 1.0],
+              colors: [
+                Theme.of(context).colorScheme.secondary,
+                Theme.of(context).colorScheme.inverseSurface,
+              ],
+            ),
+          ),
+          child: SafeArea(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    height: 80,
+                    child: Image.asset(
+                      "assets/logo_solvro.png",
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  NameHeader(
+                    color: Theme.of(context).colorScheme.onSecondary,
+                  ),
+                  const SizedBox(height: 20),
+                  const NameListTile(),
+                  const EmailListTile(),
+                  const ProfessionListTile(),
+                  const SizedBox(height: 5),
+                  const LogoutButton(),
+                  const SizedBox(height: 25),
+                ]),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 10),
+          child: Text(
+            "Twoje projekty: ",
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+        ),
+        projectsColumn,
+      ],
+    );
+    if (isLandscape) {
+      return Drawer(child: SingleChildScrollView(child: drawer));
+    }
+    return Drawer(child: drawer);
   }
 }
