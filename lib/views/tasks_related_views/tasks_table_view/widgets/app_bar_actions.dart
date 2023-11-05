81d76bdc038ca4f8696bc0980d404b843e4ca393
Szymon Kowaliński <szymon@kowalinski.dev>
2023-11-05 13:52:45 +0100

    add users action icon activation logic

--- lib/views/tasks_related_views/tasks_table_view/widgets/app_bar_actions.dart
+++ lib/views/tasks_related_views/tasks_table_view/widgets/app_bar_actions.dart
@@ -2,6 +2,8 @@ import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:solvro_mobile/services/auth_service.dart';
+import 'package:solvro_mobile/services/projects_service.dart';
 
 import '../../../../repositories/tasks_repository.dart';
 
@@ -9,15 +11,7 @@ class AppBarActions extends DelegatingList<Widget> {
   AppBarActions(BuildContext context)
       : super(
           [
-            IconButton(
-              onPressed: () {
-                context.push("/addUsers");
-              },
-              icon: const Icon(
-                Icons.person_add_alt,
-                color: Colors.white,
-              ),
-            ),
+            const AddUserActionIcon(),
             IconButton(
               onPressed: () {
                 context.push("/newTask");
@@ -30,6 +24,28 @@ class AppBarActions extends DelegatingList<Widget> {
             const RefreshIcon()
           ],
         );
+}
+
+class AddUserActionIcon extends ConsumerWidget {
+  const AddUserActionIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    if (ref.watch(authServiceProvider).value?.id !=
+            ref.watch(currentProjectSubServiceProvider).value?.owner.id ||
+        ref.watch(authServiceProvider).value == null) return Container();
+    return IconButton(
+      onPressed: () {
+        context.push("/addUsers");
+      },
+      icon: const Icon(
+        Icons.person_add_alt,
+        color: Colors.white,
+      ),
+    );
+  }
 }
 
 class RefreshIcon extends ConsumerWidget {
