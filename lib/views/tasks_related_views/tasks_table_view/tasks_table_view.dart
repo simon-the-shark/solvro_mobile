b422871e779b786f2bb5e81a61c656a111ab0d08
Szymon Kowaliński <szymon@kowalinski.dev>
2023-11-07 00:12:20 +0100

    fixed scroll

--- lib/views/tasks_related_views/tasks_table_view/tasks_table_view.dart
+++ lib/views/tasks_related_views/tasks_table_view/tasks_table_view.dart
@@ -43,56 +43,59 @@ class TaskTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        if (ResponsiveBreakpoints.of(context).largerOrEqualTo(DESKTOP))
-          const FilterDesktopSection(),
-        if (ResponsiveBreakpoints.of(context).largerOrEqualTo(DESKTOP))
-          const Divider(),
-        Stack(
-          children: [
-            Padding(
-              padding:
-                  ResponsiveBreakpoints.of(context).largerThan('HIDE_DRAWER')
-                      ? const EdgeInsets.only(
-                          top: 15, bottom: 100, left: 20, right: 20)
-                      : const EdgeInsets.only(top: 15, bottom: 100),
-              child: ResponsiveBreakpoints.of(context).largerOrEqualTo(DESKTOP)
-                  ? SizedBox(
-                      width: MediaQuery.of(context).size.width -
-                          ((ResponsiveBreakpoints.of(context)
-                                  .largerThan('HIDE_DRAWER'))
-                              ? 340
-                              : 0),
-                      child: const Column(
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          if (ResponsiveBreakpoints.of(context).largerOrEqualTo(DESKTOP))
+            const FilterDesktopSection(),
+          if (ResponsiveBreakpoints.of(context).largerOrEqualTo(DESKTOP))
+            const Divider(),
+          Stack(
+            children: [
+              Padding(
+                padding:
+                    ResponsiveBreakpoints.of(context).largerThan('HIDE_DRAWER')
+                        ? const EdgeInsets.only(
+                            top: 15, bottom: 100, left: 20, right: 20)
+                        : const EdgeInsets.only(top: 15, bottom: 100),
+                child: ResponsiveBreakpoints.of(context)
+                        .largerOrEqualTo(DESKTOP)
+                    ? SizedBox(
+                        width: MediaQuery.of(context).size.width -
+                            ((ResponsiveBreakpoints.of(context)
+                                    .largerThan('HIDE_DRAWER'))
+                                ? 340
+                                : 0),
+                        child: const Column(
+                          children: [
+                            EmptyTable(),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                TaskTypeSubtable(TaskStatusChoices.notAssigned,
+                                    wrapWithExpanded: true),
+                                TaskTypeSubtable(TaskStatusChoices.inProgress,
+                                    wrapWithExpanded: true),
+                                TaskTypeSubtable(TaskStatusChoices.closed,
+                                    wrapWithExpanded: true),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    : const Column(
                         children: [
+                          TaskTypeSubtable(TaskStatusChoices.notAssigned),
+                          TaskTypeSubtable(TaskStatusChoices.inProgress),
+                          TaskTypeSubtable(TaskStatusChoices.closed),
                           EmptyTable(),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              TaskTypeSubtable(TaskStatusChoices.notAssigned,
-                                  wrapWithExpanded: true),
-                              TaskTypeSubtable(TaskStatusChoices.inProgress,
-                                  wrapWithExpanded: true),
-                              TaskTypeSubtable(TaskStatusChoices.closed,
-                                  wrapWithExpanded: true),
-                            ],
-                          ),
                         ],
                       ),
-                    )
-                  : const Column(
-                      children: [
-                        TaskTypeSubtable(TaskStatusChoices.notAssigned),
-                        TaskTypeSubtable(TaskStatusChoices.inProgress),
-                        TaskTypeSubtable(TaskStatusChoices.closed),
-                        EmptyTable(),
-                      ],
-                    ),
-            ),
-          ],
-        ),
-      ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
